a73147a42d7eb0f07a99ccb7eb2a2dce07cf01b3
madhavisolanki <maddy9859@gmail.com>
2019-03-16 20:36:04 +0530

    Deleting pods

--- WASP/Pods/Target Support Files/Pods-WASP/Pods-WASP-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WASP : NSObject
-@end
-@implementation PodsDummy_Pods_WASP
-@end
--- WASP/Pods/Target Support Files/Pods-WASPTests/Pods-WASPTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WASPTests : NSObject
-@end
-@implementation PodsDummy_Pods_WASPTests
-@end
